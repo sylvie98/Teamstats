31ae2a8a3d3163af275ff2fbfc65f236c5b17a4f
sylvie98 <sylviemasengesho5@gmail.com>
2024-03-10 17:27:49 +0200

    team stats assignment

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
-const team = {};
+//this is team status
+const team = {
+    _players: [
+        {
+            firstame:'delphine',
+            lastname:'Alain',
+            age:19,
+        },
+       { 
+          firstName: "Alice",
+          lastName: "Smith",
+          age: 30
+       }, 
+       {
+         firstName: "Bob",
+         lastName: "Johnson",
+         age: 28
+       },
+    ],
+    _games: [
+        {
+            opponent: "Team A",
+            teamPoints: 90,
+            opponentPoints: 60
+          },
+          {
+            opponent: "Team B",
+            teamPoints: 80,
+            opponentPoints: 70
+          },
+          {
+            opponent: "Team C",
+            teamPoints: 70,
+            opponentPoints: 75
+          }
+    ],
+    get player() {
+      return this._players;
+  },
+    get games(){
+        return this._games;
+    }
 
-team._players = [];
-team._games = [];
-
-
-team._players.push({
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 25
-}, {
-    firstName: 'Jane',
-    lastName: 'Smith',
-    age: 30
-}, {
-    firstName: 'Mike',
-    lastName: 'Johnson',
-    age: 28
-});
-
-team._games.push({
-    opponent: 'Lakers',
-    teamPoints: 90,
-    opponentPoints: 85
-}, {
-    opponent: 'Knicks',
-    teamPoints: 95,
-    opponentPoints: 100
-}, {
-    opponent: 'Bulls',
-    teamPoints: 110,
-    opponentPoints: 105
-});
-
-
-team.players = function() {
-    return this._players;
 };
-
-team.games = function() {
-    return this._games;
-};
-
 team.addPlayer = function(newFirstName, newLastName, newAge) {
-    const newPlayer = {
-        firstName: newFirstName,
-        lastName: newLastName,
-        age: newAge
-    };
-    this._players.push(newPlayer);
+  const players = {
+      firstName: newFirstName,
+      lastName: newLastName,
+      age: newAge
+  };
+  this._players.push(players);
 };
+team.addgame=function(newOpponent,newTeamPoints,newOpponentPoints){
+  const games= {
+    opponent:newOpponent,
+    teamPoints: newTeamPoints,
+    opponentPoints: newOpponentPoints
+  };
+  this._games.push(games);
+}
 
-team.addPlayer('Bugs', 'Bunny', 76);
-console.log(team.players());
-
-team.addGame = function(newOpponent, newTeamPoints, newOpponentPoints) {
-    const newGame = {
-        opponent: newOpponent,
-        teamPoints: newTeamPoints,
-        opponentPoints: newOpponentPoints
-    };
-    this._games.push(newGame);
-};
-
-team.addGame('Titans', 100, 98);
-console.log(team.games());
+team.addPlayer('bugs', 'bunny', 76);
+team.addgame('Titans',100,98);
+console.log(team.player)
+console.log(team.games);
+//console.log(team);
